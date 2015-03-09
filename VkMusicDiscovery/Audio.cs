06d6d69583e1204eda5049663287e5e84882eb14
Van0SS <van0910@gmail.com>
2015-03-09 15:32:50 +0600

    Добавил объект аудиозаписи.

---
+++ VkMusicDiscovery/Audio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkMusicDiscovery
+{
+    class Audio
+    {
+        public uint Id { get; set; }
+        /// <summary>
+        /// Идентификатор владельца аудиозаписи.
+        /// </summary>
+        public int OwnerId { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public uint Duration { get; set; }
+        public string Url { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public uint LyricsId { get; set; }
+        /// <summary>
+        /// Идентификатор альбома, в котором находится аудиозапись (если присвоен).
+        /// </summary>
+        public uint AlbumId { get; set; }
+        public uint GenreId { get; set; }
+
+    }
+}
